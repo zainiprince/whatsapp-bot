bd4d77ec54b7e4669de6df822ca6086c0c2109e0
zainiprince <56239277+zainiprince@users.noreply.github.com>
2022-08-31 15:08:53 +0500

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -3,6 +3,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Raganork MD - Sourav KL11
 */
+const axios = require('axios');
 const {
      Module
  } = require('../main')
